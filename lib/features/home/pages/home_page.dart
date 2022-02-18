199bd81446e811b8b60ab51155eab7c645d61d2a
Piotr Janik <piotrjanik8@gmail.com>
2022-02-18 19:44:03 +0100

    Improve displaying dates in the app

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -146,7 +146,7 @@ class _ListViewItem extends StatelessWidget {
                           ),
                           const SizedBox(height: 10),
                           Text(
-                            itemModel.relaseDate.toString(),
+                            itemModel.relaseDateFormatted(),
                           ),
                         ],
                       ),
